edb74cbd002580abcd3223daa833388564bc0810
Joe Barnett <joebarnett876@gmail.com>
2023-12-04 11:48:50 +0000

    day4

---
+++ projects/day4/lib.h
@@ -0,0 +1,13 @@
+#include <stddef.h>
+
+typedef struct {
+  int idx;
+  size_t count;
+  int *winning_numbers;
+  size_t winning_numbers_len;
+  int *my_numbers;
+  size_t my_numbers_len;
+} Card;
+
+int part1(char *input_path);
+int part2(char *input_path);
