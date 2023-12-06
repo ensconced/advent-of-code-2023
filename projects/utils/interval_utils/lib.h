45941c2c32ad6531ac1f72947830c593074b584e
Joe Barnett <joebarnett876@gmail.com>
2023-12-06 23:04:28 +0000

    add interval utils

---
+++ projects/utils/interval_utils/lib.h
@@ -0,0 +1,11 @@
+#include <stdbool.h>
+
+typedef struct {
+  long start;
+  long end;
+} Interval;
+
+bool contains(Interval container, Interval contained);
+Interval intersection(Interval a, Interval b);
+bool is_empty(Interval interval);
+bool equals(Interval a, Interval b);
