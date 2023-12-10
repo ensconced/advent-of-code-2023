061a8fee37671c1d087d299565749f9c14a35f82
Joe Barnett <joe.barnett@elliptic.co>
2023-12-10 22:24:19 +0000

    wip

--- projects/day8/test.c
+++ projects/day8/test.c
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 int main(void) {
-  assert(part1("./inputs/example_input.txt") == 2);
-  assert(part1("./inputs/input.txt") == 18113);
+  // assert(part1("./inputs/example_input.txt") == 2);
+  // assert(part1("./inputs/input.txt") == 18113);
+  printf("%zu\n", part2("./inputs/part2_example_input.txt"));
 }
