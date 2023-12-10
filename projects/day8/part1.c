136979267b41262428417b7f4a605b8ac7d3112f
Joe Barnett <joebarnett876@gmail.com>
2023-12-10 21:23:32 +0000

    day8 part2 wip

--- projects/day8/part1.c
+++ projects/day8/part1.c
@@ -5,7 +5,7 @@
 
 size_t part1(char *input_path) {
   ParsedInput parsed_input = parse_input(input_path);
-  GraphNode *graph_nodes = create_graph(parsed_input);
+  GraphNode *graph_nodes = create_graph(parsed_input, false);
   GraphNode *start_node =
       &graph_nodes[find_node_idx_by_id(parsed_input, "AAA")];
   GraphNode *end_node = &graph_nodes[find_node_idx_by_id(parsed_input, "ZZZ")];
